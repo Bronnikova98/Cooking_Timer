43ba4cf0ed795d431dbef15157ef588ea0612c5b
Your Name <you@example.com>
2023-02-09 10:53:56 +0500

    Dumplings window

---
+++ app/src/main/java/com/example/cookingtimer/DumplingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.cookingtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DumplingsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dumplings);
+    }
+}
--- app/src/main/java/com/example/cookingtimer/MainActivity.java
+++ app/src/main/java/com/example/cookingtimer/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.cookingtimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button dumplings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dumplings = findViewById(R.id.button);
+
+        dumplings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DumplingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
