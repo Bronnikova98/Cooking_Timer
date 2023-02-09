0aae87dba3bfc10df07d04439f0d0e503d729a28
Your Name <you@example.com>
2023-02-09 21:03:23 +0500

    Made a notification in the dumplings window

--- app/src/main/java/com/example/cookingtimer/DumplingsActivity.java
+++ app/src/main/java/com/example/cookingtimer/DumplingsActivity.java
@@ -1,9 +1,16 @@
 package com.example.cookingtimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.Editable;
@@ -12,7 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Handler;
 
 public class DumplingsActivity extends AppCompatActivity {
 
@@ -20,8 +27,12 @@ public class DumplingsActivity extends AppCompatActivity {
     private MyTimer timer;
     private Button start;
     private Button reset;
-    private EditText myTime;
-
+    private EditText myTimeText;
+    private TextView exceptionText;
+    private static final int NOTIFY_ID = 100;
+    private Handler handler;
+    private NotificationManager notificationManager;
+    private final String CHANEL = "main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +42,8 @@ public class DumplingsActivity extends AppCompatActivity {
         timerView = findViewById(R.id.textView8);
         start = findViewById(R.id.button4);
         reset = findViewById(R.id.button5);
-        myTime = findViewById(R.id.myTime);
+        myTimeText = findViewById(R.id.myTime);
+        exceptionText = findViewById(R.id.exeText);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,14 +53,15 @@ public class DumplingsActivity extends AppCompatActivity {
                 }
                 String time_str = timerView.getText().toString();
                 String[] arr = time_str.split(":");
-                int min = Integer.parseInt(arr[0]);
-                int sec = Integer.parseInt(arr[1]);
-
-                long mils = (min * 60 + sec) * 1000;
-                timer = new MyTimer(mils, 1000);
-                timer.start();
-
-
+                try{
+                    int min = Integer.parseInt(arr[0]);
+                    int sec = Integer.parseInt(arr[1]);
+                    long mils = (min * 60 + sec) * 1000;
+                    timer = new MyTimer(mils, 1000);
+                    timer.start();
+                }catch (NumberFormatException nfe){
+                    exceptionText.setVisibility(View.VISIBLE);
+                }
 
             }
         });
@@ -64,10 +77,13 @@ public class DumplingsActivity extends AppCompatActivity {
                     timer.cancel();}
                 timerView.setText("00:10");
 
+                exceptionText.setVisibility(View.INVISIBLE);
+
             }
         });
 
-        myTime.addTextChangedListener(new TextWatcher() {
+        myTimeText.addTextChangedListener(new TextWatcher() {
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -83,12 +99,21 @@ public class DumplingsActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 if (timer != null){
                     timer.cancel();}
+
                 timerView.setText(s.toString());
             }
+
         });
 
-
-
+        handler = new Handler();
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(CHANEL, "Main chanel", NotificationManager.IMPORTANCE_DEFAULT);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(channel);
+        }
 
     }
 
@@ -104,8 +129,6 @@ public class DumplingsActivity extends AppCompatActivity {
             sec -= min * 60;
 
             timerView.setText(String.format("%02d:%02d", min,sec));
-
-
         }
 
         @Override
@@ -114,12 +137,27 @@ public class DumplingsActivity extends AppCompatActivity {
             timerView.setTextColor(Color.parseColor("#873939"));
             start.setText("Готово!");
             start.getBackground().setColorFilter(Color.parseColor("#873939"), PorterDuff.Mode.MULTIPLY);
-            Toast toast = Toast.makeText(getApplicationContext(), "Время вышло", Toast.LENGTH_SHORT);
-            toast.show();
 
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent notificationIntent = new Intent(DumplingsActivity.this, DumplingsActivity.class);
+                    PendingIntent contentIntent = PendingIntent.getActivity(DumplingsActivity.this, 0, notificationIntent, 0);
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(DumplingsActivity.this, CHANEL)
+                            .setSmallIcon(R.drawable.timericon)
+                            .setContentTitle("Время вышло")
+                            .setChannelId(CHANEL)
+                            .setContentText("Пора переходить к следующему шагу")
+                            .setContentIntent(contentIntent)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+                    Notification notification = builder.build();
+                    notification.defaults = Notification.DEFAULT_ALL;
+
+                    notificationManager.notify(NOTIFY_ID, builder.build());
+                }
+            }, 5000);
 
         }
     }
-
-
 }
